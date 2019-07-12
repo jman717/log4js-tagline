9fd21a17b9d59d702359341d2b31aea41d7da1b5
Jim Manton <jrman@risebroadband.net>
2019-07-12 14:53:04 -0600

    Update test.

--- test/package.js
+++ test/package.js
@@ -4,7 +4,7 @@ const assert = require('assert'),
 
 const packageMock = {
   "_from": "log4js-tagline",
-  "_id": "log4js-tagline@2.1.10",
+  "_id": "log4js-tagline@2.1.12",
   "_inBundle": false,
   "_integrity": "sha512-hBdHoL8qLKlK0bxw+BnD5F3XFJgRMyog6tq/BRJ34OIDhIBxY5KrHw21dRVdW1P232j1zf/zlHci9w3XkzATQQ==",
   "_location": "/log4js-tagline",
@@ -24,7 +24,7 @@ const packageMock = {
     "/",
     "/email-smtp-cron-delivery"
   ],
-  "_resolved": "https://registry.npmjs.org/log4js-tagline/-/log4js-tagline-2.1.10.tgz",
+  "_resolved": "https://registry.npmjs.org/log4js-tagline/-/log4js-tagline-2.1.12.tgz",
   "_shasum": "4ec25a7774ff9d76ab8d42ebed713ee7f2ec610a",
   "_spec": "log4js-tagline",
   "_where": "C:\\Users\\jmanton\\node\\managed giving\\processachacknowledgment",
@@ -36,8 +36,8 @@ const packageMock = {
     "chai": "^4.2.0",
     "colors": "*",
     "compare-json-difference": "^0.1.3",
-    "email-smtp-cron-delivery": "0.0.10",
-    "log4js": "^4.4.0",
+    "email-smtp-cron-delivery": "0.0.11",
+    "log4js": "^4.5.1",
     "mocha": "^6.1.4",
     "node-dogstatsd": "0.0.7"
   },
@@ -60,7 +60,7 @@ const packageMock = {
   "main": "app.js",
   "name": "log4js-tagline",
   "start": "node app.js",
-  "version": "2.1.10"
+  "version": "2.1.12"
 }
 
 describe('package.json', function () {
