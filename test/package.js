ee21f85fb080dcaa2e9855f6a4bd641267de61e1
Jim Manton <jrman@risebroadband.net>
2019-07-16 10:55:25 -0600

    Updates to versions.

--- test/package.js
+++ test/package.js
@@ -3,31 +3,6 @@ const assert = require('assert'),
     packagejson = require('../package.json')
 
 const packageMock = {
-  "_from": "log4js-tagline",
-  "_id": "log4js-tagline@2.1.12",
-  "_inBundle": false,
-  "_integrity": "sha512-hBdHoL8qLKlK0bxw+BnD5F3XFJgRMyog6tq/BRJ34OIDhIBxY5KrHw21dRVdW1P232j1zf/zlHci9w3XkzATQQ==",
-  "_location": "/log4js-tagline",
-  "_phantomChildren": {},
-  "_requested": {
-    "type": "tag",
-    "registry": true,
-    "raw": "log4js-tagline",
-    "name": "log4js-tagline",
-    "escapedName": "log4js-tagline",
-    "rawSpec": "",
-    "saveSpec": null,
-    "fetchSpec": "latest"
-  },
-  "_requiredBy": [
-    "#USER",
-    "/",
-    "/email-smtp-cron-delivery"
-  ],
-  "_resolved": "https://registry.npmjs.org/log4js-tagline/-/log4js-tagline-2.1.12.tgz",
-  "_shasum": "4ec25a7774ff9d76ab8d42ebed713ee7f2ec610a",
-  "_spec": "log4js-tagline",
-  "_where": "C:\\Users\\jmanton\\node\\managed giving\\processachacknowledgment",
   "author": {
     "name": "Jim Manton"
   },
@@ -36,7 +11,7 @@ const packageMock = {
     "chai": "^4.2.0",
     "colors": "*",
     "compare-json-difference": "^0.1.3",
-    "email-smtp-cron-delivery": "0.0.11",
+    "email-smtp-cron-delivery": "0.0.12",
     "log4js": "^4.5.1",
     "mocha": "^6.1.4",
     "node-dogstatsd": "0.0.7"
@@ -60,7 +35,7 @@ const packageMock = {
   "main": "app.js",
   "name": "log4js-tagline",
   "start": "node app.js",
-  "version": "2.1.12"
+  "version": "2.1.13"
 }
 
 describe('package.json', function () {
